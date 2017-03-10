5bf59fa39bfa4a234e2274f944e132a4d931b54a
Jonathan Braun <Jonathanrbraun@gmail.com>
2017-03-10 11:57:01 -0500

    add paula scher and update lumography

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'portfolio#index'
+  get '/paula_scher', to: 'portfolio#ps'
+  get '/boston_boxed_bread', to: 'portfolio#bbb'
+
 
 end

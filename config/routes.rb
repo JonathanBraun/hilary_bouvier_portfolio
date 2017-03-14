c8f1a19120c651e889e875e5496cfc237f5c2ca2
Jonathan Braun <Jonathanrbraun@gmail.com>
2017-03-14 12:38:59 -0400

    add morris fuller benton page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   get '/boston_boxed_bread', to: 'portfolio#bbb'
   get '/globe_to_glass', to: 'portfolio#gtg'
   get '/happiness_hypothesis', to: 'portfolio#hh'
-
+  get '/morris_fuller_benton', to: 'portfolio#mfb'
 
 end

422953bb1845593b0400351c672a38d5c14b86a2
Jonathan Braun <Jonathanrbraun@gmail.com>
2017-03-20 16:31:41 -0400

    index work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,11 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+
+var debugInput = document.querySelector("input");
+function updateDebugState() {
+    document.body.classList.toggle('debug-on', debugInput.checked);
+}
+debugInput.addEventListener("click", updateDebugState);
+updateDebugState();
